f837d9d28ddf01dc7af8f76f0c477b4b0e567252
Note <hh33ee33@gmail.com>
2022-11-23 20:30:04 +0900

    Revert "bug fixes"
    This reverts commit 3edf8a55967b0103995d9c3830f107f9fd1a82f7.

--- src/App.js
+++ src/App.js
@@ -9,16 +9,9 @@ import EventModal from "./components/EventModal";
 import Snowfall from 'react-snowfall';
 
 function App() {
-
-  const snowflake1 = document.createElement('img')
-  snowflake1.src = 'https://ifh.cc/g/VlpvWk.png'
-  const snowflake2 = document.createElement('img')
-  snowflake2.src = 'https://ifh.cc/g/VlpvWk.png'
-
-  const images = [snowflake1, snowflake2]
-
   const [currenMonth, setCurrentMonth] = useState(getMonth());
   const { monthIndex, showEventModal } = useContext(GlobalContext);
+
 
   useEffect(() => {
     setCurrentMonth(getMonth(monthIndex));
@@ -33,33 +26,17 @@ function App() {
           <Sidebar />
           <Month month={currenMonth} />
           {
-            monthIndex%12+1 === 11?
-            <Snowfall snowflakeCount={200}/>
-            :monthIndex%12+1 === 12?
+            monthIndex%12+1 === 12?
             <Snowfall snowflakeCount={200} />
             :monthIndex%12+1 === 1?
             <Snowfall snowflakeCount={200} />
             :monthIndex%12+1 === 2?
-            <Snowfall snowflakeCount={200} />
-            :monthIndex%12+1 === -1?
-            <Snowfall snowflakeCount={200}/>
-            :monthIndex%12+1 === 0?
-            <Snowfall snowflakeCount={200} />
-            :monthIndex%12+1 === -10?
-            <Snowfall snowflakeCount={200} />
-            :monthIndex%12+1 === -11?
             <Snowfall snowflakeCount={200} />
             :monthIndex%12+1 === 3?
             <Snowfall color = "pink" snowflakeCount={200} />
             :monthIndex%12+1 === 4?
             <Snowfall color = "pink" snowflakeCount={200} />
             :monthIndex%12+1 === 5?
-            <Snowfall color = "pink" snowflakeCount={200} />
-            :monthIndex%12+1 === -7?
-            <Snowfall color = "pink" snowflakeCount={200} />
-            :monthIndex%12+1 === -8?
-            <Snowfall color = "pink" snowflakeCount={200} />
-            :monthIndex%12+1 === -9?
             <Snowfall color = "pink" snowflakeCount={200} />
             :null
           }
